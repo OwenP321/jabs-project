3c4b2afc89f46188ae26d0156cc7522a4f9fc164
Owen Purdie <owenwpurdie@gmail.com>
2024-01-31 12:37:25 +0000

    Setting up normal PBFT

--- src/main/java/jabs/Main.java
+++ src/main/java/jabs/Main.java
@@ -64,7 +64,13 @@ public class Main {
 
         System.out.println("PBFT CONCENSUS");
 
-        scenario = new PBFTLANScenario("One hour in the life of PBFT", 1, 40, 3600);
-        scenario.AddNewLogger(new BlockPropagationDelayLogger(Paths.get("Output/PBFTLAN-50-propagation-delay-log.csv"), 0.5));
+        //scenario = new PBFTLANScenario("One hour in the life of PBFT", 1, 40, 3600);
+        //scenario.AddNewLogger(new BlockPropagationDelayLogger(Paths.get("Output/PBFTLAN-50-propagation-delay-log.csv"), 0.5));
+
+        scenario = new PBFTLANScenario("One hour of a PBFT lan Network", 1,
+        40, 3600);
+        scenario.AddNewLogger(new PBFTCSVLogger(Paths.get("output/pbft-simulation-log.csv")));
+        scenario.run();
+
 }
 }
