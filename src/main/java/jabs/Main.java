2e5970276f9b0b3b3469bfe929984f02143a046a
Owen Purdie <owenwpurdie@gmail.com>
2024-02-05 12:59:44 +0000

    added a new logger

--- src/main/java/jabs/Main.java
+++ src/main/java/jabs/Main.java
@@ -70,7 +70,7 @@ public class Main {
 
         scenario = new PBFTLANScenario("One hour of a PBFT lan Network", 1,40, 3600);
         scenario.AddNewLogger(new PBFTCSVLogger(Paths.get("output/pbft-simulation-log.csv")));
-        
+        scenario.AddNewLogger(new BlockConfirmationLogger(Paths.get("output/pbft-Block-confirmation-log.csv ")));
         scenario.run();
 
 }
