5c3d3ad35c714df9c2bb57727fe81aaebac6d0a5
Owen Purdie <owenwpurdie@gmail.com>
2024-02-05 17:47:56 +0000

    Added some transactions to pbft block

--- src/main/java/jabs/ledgerdata/pbft/PBFTBlock.java
+++ src/main/java/jabs/ledgerdata/pbft/PBFTBlock.java
@@ -1,12 +1,35 @@
 package jabs.ledgerdata.pbft;
 
+import java.util.ArrayList;
+
 import jabs.ledgerdata.SingleParentBlock;
 import jabs.network.node.nodes.Node;
 
 public class PBFTBlock extends SingleParentBlock<PBFTBlock> {
     public static final int PBFT_BLOCK_HASH_SIZE = 32;
 
+    protected ArrayList<PBFTTx> transactions;
+    
+
     public PBFTBlock(int size, int height, double creationTime, Node creator, PBFTBlock parent) {
         super(size, height, creationTime, creator, parent, PBFT_BLOCK_HASH_SIZE);
     }
+
+    public void addTransaction(PBFTTx tx)
+    {
+        this.transactions.add(tx);
+    }
+
+    public void setTransactions(ArrayList<PBFTTx> transactions)
+    {
+        this.transactions = transactions;
+    }
+    
+    public ArrayList<PBFTTx> getTransactions()
+    {
+        return this.transactions;
+    }
+
+    
+
 }
