e23df2c9df706b7d8f1d38c6cd2bd10f2dc2ea01
owen purdie <owenwpurdie@gmail.com>
2024-03-01 12:16:27 +0000

    Added a new PBFTTxVote consensus to work on getting the transaction order correct

--- src/main/java/jabs/consensus/algorithm/PBFTTxVote.java
+++ src/main/java/jabs/consensus/algorithm/PBFTTxVote.java
@@ -12,58 +12,185 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+// based on: https://sawtooth.hyperledger.org/docs/pbft/nightly/master/architecture.html
+// another good source: http://ug93tad.github.io/pbft/
+
 public class PBFTTxVote<B extends SingleParentBlock<B>> extends AbstractChainBasedConsensus<B, EthereumTx>
         implements VotingBasedConsensus<B, EthereumTx >, DeterministicFinalityConsensus<B, EthereumTx > {
-            private final int numAllParticipants;
-            private final HashMap<B, HashMap<Node, Vote>> prepareVotes = new HashMap<>();
-            private final HashMap<B, HashMap<Node, Vote>> commitVotes = new HashMap<>();
-            private final HashSet<B> preparedBlocks = new HashSet<>();
-            private final HashSet<B> committedBlocks = new HashSet<>();
-            private int currentViewNumber = 0;
-            
-            
+    private final int numAllParticipants;
+    private final HashMap<B, HashMap<Node, Vote>> prepareVotes = new HashMap<>();
+    private final HashMap<B, HashMap<Node, Vote>> commitVotes = new HashMap<>();
+    private final HashSet<B> preparedBlocks = new HashSet<>();
+    private final HashSet<B> committedBlocks = new HashSet<>();
+    private int currentViewNumber = 0;
+
+    // TODO: View change should be implemented
+
+    private PBFTMode pbftMode = PBFTMode.NORMAL_MODE;
+    private PBFTPhase pbftPhase = PBFTPhase.PRE_PREPARING;
+
+    ArrayList<EthereumTx> txOrder = new ArrayList<EthereumTx>();
+    ArrayList<EthereumTx> finalOrder = new ArrayList<EthereumTx>();
+
+    @Override
+    public boolean isBlockFinalized(B block) {
+        return false;
+    }
+
+    @Override
+    public boolean isTxFinalized(EthereumTx tx) {
+        return false;
+    }
+
+    @Override
+    public int getNumOfFinalizedBlocks() {
+        return 0;
+    }
+
+    @Override
+    public int getNumOfFinalizedTxs() {
+        return 0;
+    }
+
+    public enum PBFTMode {
+        NORMAL_MODE,
+        VIEW_CHANGE_MODE
+    }
+
+    public enum PBFTPhase {
+        PRE_PREPARING,
+        PREPARING,
+        COMMITTING
+    }
+
     public PBFTTxVote(LocalBlockTree<B> localBlockTree, int numAllParticipants) {
         super(localBlockTree);
         this.numAllParticipants = numAllParticipants;
         this.currentMainChainHead = localBlockTree.getGenesisBlock();
     }
 
+    public void newIncomingVote(Vote vote) {
+        if (vote instanceof PBFTBlockVote) { // for the time being, the view change votes are not supported
+            PBFTBlockVote<B> blockVote = (PBFTBlockVote<B>) vote;
+            B block = blockVote.getBlock();
+            switch (blockVote.getVoteType()) {
+                case PRE_PREPARE :
+                    if (!this.localBlockTree.contains(block)) {
+                        this.localBlockTree.add(block);
+                    }
+                    if (this.localBlockTree.getLocalBlock(block).isConnectedToGenesis) {
+                        this.pbftPhase = PBFTPhase.PREPARING;
+                        this.peerBlockchainNode.broadcastMessage(
+                                new VoteMessage(
+                                        new PBFTPrepareVote<>(this.peerBlockchainNode, blockVote.getBlock())
+                                )
+                        );
+                    }
+                    break;
+                case PREPARE:
+                    checkVotes(blockVote, block, prepareVotes, preparedBlocks, PBFTPhase.COMMITTING);
+                    break;
+                case COMMIT:
+                    checkVotes(blockVote, block, commitVotes, committedBlocks, PBFTPhase.PRE_PREPARING);
+                    break;
+            }
+        }
+    }
+
+    private void checkVotes(PBFTBlockVote<B> vote, B block, HashMap<B, HashMap<Node, Vote>> votes, HashSet<B> blocks, PBFTPhase nextStep) {
+        if (!blocks.contains(block)) {
+            if (!votes.containsKey(block)) { // this the first vote received for this block
+                votes.put(block, new HashMap<>());
+            }
+            votes.get(block).put(vote.getVoter(), vote);
+            if (votes.get(block).size() > (((numAllParticipants / 3) * 2) + 1)) {
+                blocks.add(block);
+                this.pbftPhase = nextStep;
+                switch (nextStep) {
+                    case PRE_PREPARING:
+                        this.currentViewNumber += 1;
+                        this.currentMainChainHead = block;
+                        updateChain();
+                        if (this.peerBlockchainNode.nodeID == this.getCurrentPrimaryNumber()){
+                            this.peerBlockchainNode.broadcastMessage(
+                                    new VoteMessage(
+                                            new PBFTPrePrepareVote<>(this.peerBlockchainNode,
+                                                    BlockFactory.samplePBFTBlock(peerBlockchainNode.getSimulator(),
+                                                            peerBlockchainNode.getNetwork().getRandom(),
+                                                            (PBFTNode) this.peerBlockchainNode, (PBFTBlock) block)
+                                            )
+                                    )
+                            );
+                        }
+                        break;
+                    case COMMITTING:
+                        this.peerBlockchainNode.broadcastMessage(
+                                new VoteMessage(
+                                        new PBFTCommitVote<>(this.peerBlockchainNode, block)
+                                )
+                        );
+                        break;
+                }
+            }
+        }
+    }
+
     @Override
     public void newIncomingBlock(B block) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'newIncomingBlock'");
+        
+        if(block instanceof PBFTBlock){
+            PBFTBlock pbftBlock = (PBFTBlock) block;
+            
+            if(!isBlockConfirmed(block) && !isBlockFinalized(block)){
+                
+                for (EthereumTx tx : pbftBlock.getTransactions()) {
+                    PBFTTransactionVote<EthereumTx> txVote =  new PBFTTransactionVote<>(10, peerBlockchainNode, tx);
+                    
+                    this.peerBlockchainNode.broadcastMessage(new VoteMessage(txVote));
+                }
+            }
+            
+        }
     }
+
+    /**
+     * @param block
+     * @return
+     */
     @Override
-    public boolean isBlockFinalized(B block) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isBlockFinalized'");
+    public boolean isBlockConfirmed(B block) {
+        return confirmedBlocks.contains(block);
     }
+
+    /**
+     * @param block
+     * @return
+     */
     @Override
-    public boolean isTxFinalized(EthereumTx tx) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isTxFinalized'");
+    public boolean isBlockValid(B block) {
+        return true;
     }
-    @Override
-    public int getNumOfFinalizedBlocks() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNumOfFinalizedBlocks'");
+
+    public int getCurrentViewNumber() {
+        return this.currentViewNumber;
     }
-    @Override
-    public int getNumOfFinalizedTxs() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNumOfFinalizedTxs'");
+
+    public int getCurrentPrimaryNumber() {
+        return (this.currentViewNumber % this.numAllParticipants);
     }
-    @Override
-    public void newIncomingVote(Vote vote) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'newIncomingVote'");
+
+    public int getNumAllParticipants() {
+        return this.numAllParticipants;
     }
+
+    public PBFTPhase getPbftPhase() {
+        return this.pbftPhase;
+    
+    }
+
     @Override
     protected void updateChain() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateChain'");
+        this.confirmedBlocks.add(this.currentMainChainHead);
     }
-
-
 
 }
