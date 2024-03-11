f886dc1f5376788c8b5fd0239cf8cb967c8844d5
owen purdie <owenwpurdie@gmail.com>
2024-03-11 17:47:51 +0000

    Testing putEvents for the loggers

---
+++ src/main/java/jabs/log/TransactionLogger.java
@@ -0,0 +1,12 @@
+package jabs.log;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import jabs.ledgerdata.ethereum.EthereumTx;
+import jabs.simulator.event.Event;
+import jabs.simulator.event.*;
+
+public class TransactionLogger {
+    
+}
